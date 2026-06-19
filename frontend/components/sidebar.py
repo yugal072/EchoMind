8765785da8795a4c7782a0eda83041f81aced850
yugal072 <yugalupadhyay313@gmail.com>
2026-06-19 23:39:57 +0530

    adding history view in sidebar

--- frontend/components/sidebar.py
+++ frontend/components/sidebar.py
@@ -27,3 +27,21 @@ def render_sidebar():
         if st.button("Clear Chat"):
             st.session_state.messages = []
             st.rerun()
+            
+        ## Session HIstory view
+        st.divider()
+        st.subheader("History")
+        
+        if st.button("Show Full History"):
+            if "messages" in st.session_state and st.session_state.messages:
+                with st.expander("Raw Chat History", expanded= True):
+                    st.json(st.session_state.messages)
+            else:
+                st.info("No messages yet")
+                
+        if st.button("Clear Chat History"):
+            st.session_state.messages = []
+            st.rerun
+        
+        if "session_id" in st.session_state:
+            st.caption(f"Session ID: {st.session_state.session_id}")
