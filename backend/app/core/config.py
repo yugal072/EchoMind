a093bc53794deee091313e9dd7ee54fcf74cbc9e
yugal072 <yugalupadhyay313@gmail.com>
2026-06-26 01:05:39 +0530

    adding upload dirs

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,9 +5,13 @@ BASE_DIR = Path(__file__).resolve().parents[2]
 
 DATA_DIR = BASE_DIR / "data"
 
-FILES_DIR = DATA_DIR / "files"
+DUMPS_DIR = DATA_DIR / "dumps"
 
-DUMPS_DIR = FILES_DIR / "dumps"
+UPLOAD_DIR = DATA_DIR / "uploads"
+
+FILES_DIR = UPLOAD_DIR / "files"
+
+EMAIL_DIR = UPLOAD_DIR / "emails"
 
 VECTORSTORE_DIR = DATA_DIR / "vectorstore"
 
